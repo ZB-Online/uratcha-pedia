8ecfa920a4d9f4c165dc24dc67d4937084fce93a
Seungyeon Lee <sie9412@gmail.com>
2021-12-10 19:36:50 +0900

    chore: add package manager, webpack setting

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const compiler = webpack(webpackConfig);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+  })
+);
+
+app.set('port', PORT);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+// const NodemonPlugin = require('nodemon-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', './public/js/app.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, './public/js/app.js')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    port: 5000,
+    open: true,
+    // proxy: {
+    //   "/api": "http://localhost:8080"
+    // },
+  },
+};
