90f7427685dcbb09615ff5241605e28cf23ee4b7
Seungyeon Lee <sie9412@gmail.com>
2021-12-13 23:45:06 +0900

    Feat: #7 router

--- public/js/app.js
+++ public/js/app.js
@@ -9,59 +9,10 @@ import '../css/search-result.css';
 // import watchaLogo from '../img/watcha_logo.png';
 // import poster from '../img/poster.jpeg';
 
-import { init, initialRoute, router } from './router';
+import { eventListeners, init, initialRoute, router } from './router';
 
 initialRoute();
 
-const eventListeners = () => {
-  const $headerLogo = document.querySelector('header .logo');
-  const $serachForm = document.querySelector('.search-form');
-  const $myPageBtn = document.querySelector('.my-page .btn');
-  const $signInBtn = document.querySelector('.sign-in .btn');
-  const $signUpBtn = document.querySelector('.sign-up .btn');
-  const $signModal = document.querySelector('.sign-modal');
-  const $signInModal = document.querySelector('.signin-modal');
-  const $signUpModal = document.querySelector('.signup-modal');
-  const $movieContainer = document.querySelector('.movie-container ul');
-
-  $headerLogo.onclick = () => {
-    const route = $headerLogo.getAttribute('route');
-    router(route, '/');
-  };
-
-  $serachForm.onsubmit = e => {
-    e.preventDefault();
-    const route = $serachForm.getAttribute('route');
-    router(route, '/search');
-  };
-
-  $signInBtn.onclick = () => {
-    $signModal.classList.remove('hidden');
-    $signInModal.classList.remove('hidden');
-    $signUpModal.classList.add('hidden');
-  };
-
-  $signUpBtn.onclick = () => {
-    $signModal.classList.remove('hidden');
-    $signUpModal.classList.remove('hidden');
-    $signInModal.classList.add('hidden');
-  };
-
-  $movieContainer.onclick = ({ target }) => {
-    if (!target.matches('.movie-item *')) return;
-    const route = '/detail';
-    router(route, '/detail');
-  };
-
-  $myPageBtn.onclick = () => {
-    const route = '/mypage';
-    router(route, '/mypage');
-  };
-};
-
 window.onload = () => {
-  // init(() => {
-  //   router(location.pathname, location.pathname);
-  // });
   eventListeners();
 };
--- public/js/router.js
+++ public/js/router.js
@@ -3,8 +3,6 @@ import Detail from './pages/MovieDetail';
 import MyPage from './pages/MyPage';
 import SearchResult from './pages/Search';
 
-const ROUTE_CHANGE_EVENT = 'ROUTE_CHANGE';
-
 const routes = {
   '/': Home,
   '/home': Home,
@@ -22,18 +20,58 @@ function render() {
 export function initialRoute() {
   render(routes['/']);
   window.onpopstate = () => {
-    render(routes[window.location.pathname]);
+    render();
   };
 }
 
-// export const init = onRouteChange => {
-//   window.addEventListener(ROUTE_CHANGE_EVENT, () => {
-//     onRouteChange();
-//   });
-// };
+export const eventListeners = () => {
+  const $headerLogo = document.querySelector('header .logo');
+  const $serachForm = document.querySelector('.search-form');
+  const $myPageBtn = document.querySelector('.my-page .btn');
+  const $signInBtn = document.querySelector('.sign-in .btn');
+  const $signUpBtn = document.querySelector('.sign-up .btn');
+  const $signModal = document.querySelector('.sign-modal');
+  const $signInModal = document.querySelector('.signin-modal');
+  const $signUpModal = document.querySelector('.signup-modal');
+  const $movieContainer = document.querySelector('.movie-container ul');
+
+  $headerLogo.onclick = () => {
+    const route = $headerLogo.getAttribute('route');
+    router(route);
+  };
+
+  $serachForm.onsubmit = e => {
+    e.preventDefault();
+    const route = $serachForm.getAttribute('route');
+    router(route);
+  };
+
+  $signInBtn.onclick = () => {
+    $signModal.classList.remove('hidden');
+    $signInModal.classList.remove('hidden');
+    $signUpModal.classList.add('hidden');
+  };
+
+  $signUpBtn.onclick = () => {
+    $signModal.classList.remove('hidden');
+    $signUpModal.classList.remove('hidden');
+    $signInModal.classList.add('hidden');
+  };
+
+  $movieContainer.onclick = ({ target }) => {
+    if (!target.matches('.movie-item *')) return;
+    const route = '/detail';
+    router(route);
+  };
+
+  $myPageBtn.onclick = () => {
+    const route = '/mypage';
+    router(route);
+  };
+};
 
 export function router(pathName) {
   window.history.pushState({}, pathName, window.location.origin + pathName);
   render();
-  // window.dispatchEvent(new CustomEvent(ROUTE_CHANGE_EVENT));
+  eventListeners();
 }
