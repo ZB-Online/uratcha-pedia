38dcd529fccb7c7c64f5ec0dcb17c30a28284ea2
jnhro1 <jnhro1@gmail.com>
2021-12-14 20:55:16 +0900

    #17 movie api

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -6,7 +6,6 @@ const getPopularMovies = async (page = 1) => {
   let data = [];
   try {
     const response = await fetch(`${apiBaseUrl}movie/popular?api_key=${apiKey}&page=${page}`);
-    console.log('responseData', response);
     const responseData = await response.json();
     data = responseData?.results.map(movie => ({
       id: movie.id,
@@ -15,35 +14,34 @@ const getPopularMovies = async (page = 1) => {
       poster_path: movie.poster_path,
       release_date: movie.release_date,
     }));
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
   return data;
 };
 
-function findCertification(certificationData, country) {
+const findCertification = async (certificationData, country) => {
   const matchedReleaseDates = certificationData.find(item => item.iso_3166_1 === country);
   const result = matchedReleaseDates?.release_dates.find(date => date.certification !== '');
   return result ? result.certification : 'None';
-}
+};
 
-const getMoviesMainDetails = async movieId => {
+const getMoviesDetailsById = async movieId => {
   let data = {};
   try {
-    // 몇가지
     const response = await fetch(`${apiBaseUrl}movie/${movieId}?api_key=${apiKey}`);
     const responseData = await response.json();
-    // 출연진
     const responseCredits = await fetch(`${apiBaseUrl}movie/${movieId}/credits?api_key=${apiKey}`);
     const responseCreditsData = await responseCredits.json();
-    // 연령
     const responseCertification = await fetch(`${apiBaseUrl}movie/${movieId}/release_dates?api_key=${apiKey}`);
     const responseCertificationData = await responseCertification.json();
-
     const country = responseData?.production_countries.map(country => country.iso_3166_1)[0];
     const certification = findCertification(responseCertificationData?.results, country);
 
     data = {
+      id: responseData?.id,
+      title: responseData?.title,
+      overview: responseData?.overview,
+      poster_path: responseData?.poster_path,
+      release_date: responseData?.release_date,
       genres: responseData?.genres.map(genre => genre.name),
       country,
       runtime: responseData?.runtime,
@@ -53,11 +51,25 @@ const getMoviesMainDetails = async movieId => {
         character: cast.character,
       })),
     };
+  } catch (error) {
+    console.log(error);
+  }
+  return data;
+};
+
+const getMoviesMainDetails = async movieId => {
+  let data = {};
+  try {
+    const response = await fetch(`${apiBaseUrl}movie/${movieId}?api_key=${apiKey}`);
+    const responseData = await response.json();
+    data = {
+      country: responseData?.production_countries.map(country => country.iso_3166_1)[0],
+    };
   } catch (error) {}
   return data;
 };
 
-const getBoxOfficeMovies = async movies => {
+const getMoviesWithCountry = async movies => {
   let data = [];
   for (let movie of movies) {
     const additionalInfo = await getMoviesMainDetails(movie.id);
@@ -66,8 +78,22 @@ const getBoxOfficeMovies = async movies => {
   return data;
 };
 
+const searchMoviesById = async keyword => {
+  let data = {};
+  try {
+    const response = await fetch(`${apiBaseUrl}search/movie?query=${keyword}&api_key=${apiKey}`);
+    const responseData = await response.json();
+    data = responseData
+  } catch (error) {
+    console.log(error);
+  }
+  return data;
+};
+
 module.exports = {
   getPopularMovies,
+  getMoviesDetailsById,
   getMoviesMainDetails,
-  getBoxOfficeMovies,
+  getMoviesWithCountry,
+  searchMoviesById
 };
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -3,10 +3,22 @@ const express = require('express');
 const router = express.Router();
 const movieCtrl = require('../controllers/movies');
 
-router.get('/', async(req, res) => {
-  const data = await movieCtrl.getPopularMovies()
-  console.log(data)
-  res.send('hello movies');
+router.get('/', async (req, res) => {
+  const data = await movieCtrl.getPopularMovies();
+  const data2 = await movieCtrl.getMoviesWithCountry(data);
+  res.send(data2);
+});
+
+router.get('/:movieId', async (req, res) => {
+  const { movieId } = req.params;
+  const data = await movieCtrl.getMoviesDetailsById(movieId);
+  res.send(data);
+});
+
+router.get('/search/:keyword', async (req, res) => {
+  const { keyword } = req.params;
+  const data = await movieCtrl.searchMoviesById(keyword);
+  res.send(data);
 });
 
 module.exports = router;
