b0b7060efa1b2eee20eafb778788cc6b184ed21b
jnhro1 <jnhro1@gmail.com>
2021-12-21 15:04:55 +0900

    #59 fix

--- front/src/js/utils/cookie.js
+++ front/src/js/utils/cookie.js
@@ -1,21 +1,3 @@
-// const getCookieValue = () => {
-//   let cookieKey = 'x_auth=';
-//   let result = '';
-//   const cookieArr = document.cookie.split(';');
-
-//   for (let i = 0; i < cookieArr.length; i++) {
-//     if (cookieArr[i][0] === ' ') {
-//       cookieArr[i] = cookieArr[i].substring(1);
-//     }
-
-//     if (cookieArr[i].indexOf(cookieKey) === 0) {
-//       result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
-//       return result;
-//     }
-//   }
-//   return result;
-// };
-
 const setCookieValue = (token) => {
   return localStorage.setItem('accessToken',token);
 };
@@ -27,12 +9,5 @@ const getCookieValue = () => {
 const delCookie = () => {
   return localStorage.removeItem('accessToken');
 };
-
-// const delCookie = function delCookie_x_auth() {
-//   let date = new Date();
-//   date.setDate(date.getDate() - 100);
-//   let Cookie = `x_auth=;Expires=${date.toUTCString()}`;
-//   document.cookie = Cookie;
-// };
 
 export { getCookieValue, delCookie, setCookieValue };
